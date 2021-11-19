eda1de0b782f0d4bb1c2103bb6d66e9278fb76fd
z88243310 <z88243310@gmail.com>
2021-11-19 23:39:46 +0800

    feat: add handlebars

--- app.js
+++ app.js
@@ -1,12 +1,46 @@
 // 載入 express 並建構應用程式伺服器
 const express = require('express')
+const exphbs = require('express-handlebars')
 const app = express()
-// 設定首頁路由
+const PORT = 3000
 
+const users = [
+  {
+    firstName: 'Tony',
+    email: 'tony@stark.com',
+    password: 'iamironman',
+  },
+  {
+    firstName: 'Steve',
+    email: 'captain@hotmail.com',
+    password: 'icandothisallday',
+  },
+  {
+    firstName: 'Peter',
+    email: 'peter@parker.com',
+    password: 'enajyram',
+  },
+  {
+    firstName: 'Natasha',
+    email: 'natasha@gamil.com',
+    password: '*parol#@$!',
+  },
+  {
+    firstName: 'Nick',
+    email: 'nick@shield.com',
+    password: 'password',
+  },
+]
+
+// handlebars
+app.engine('hbs', exphbs({ defaultLayout: 'main', extname: '.hbs' }))
+app.set('view engine', 'hbs')
+
+// 設定首頁路由
 app.get('/', (req, res) => {
-  res.send('hello world')
+  res.render('index')
 })
 // 設定 port 3000
-app.listen(3000, () => {
+app.listen(PORT, () => {
   console.log('App is running on http://localhost:3000')
 })
